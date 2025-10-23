63522e3076264794d78d9f53a6c37e6db44b3fb4
saltchicken <john.eicher89@gmail.com>
2025-10-23 11:52:32 -0700

    Use elgato-streamdeck crate

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,158 @@
-use hidapi::HidApi;
+use image::open;
+use std::time::Duration;
 
-// Define Elgato's Vendor ID as a constant.
-const ELGATO_VENDOR_ID: u16 = 0x0fd9;
+use elgato_streamdeck::images::{ImageRect, convert_image_with_format};
+use elgato_streamdeck::{AsyncStreamDeck, DeviceStateUpdate, list_devices, new_hidapi};
+use tokio::time::sleep;
 
-fn main() {
-    println!("Searching for a Stream Deck...");
+#[tokio::main]
+async fn main() {
+    // Create instance of HidApi
+    match new_hidapi() {
+        Ok(hid) => {
+            // Refresh device list
+            for (kind, serial) in list_devices(&hid) {
+                println!("{:?} {} {}", kind, serial, kind.product_id());
 
-    // Initialize the HIDAPI. If this fails, it's likely due to a missing
-    // system library (like libhidapi-dev on Debian/Ubuntu).
-    let api = HidApi::new().expect("Failed to initialize HIDAPI");
+                // Connect to the device
+                let device =
+                    AsyncStreamDeck::connect(&hid, kind, &serial).expect("Failed to connect");
+                // Print out some info from the device
+                println!(
+                    "Connected to '{}' with version '{}'",
+                    device.serial_number().await.unwrap(),
+                    device.firmware_version().await.unwrap()
+                );
 
-    // A flag to track if we found the device.
-    let mut deck_found = false;
+                device.set_brightness(50).await.unwrap();
+                device.clear_all_button_images().await.unwrap();
 
-    // Iterate over all connected HID devices.
-    for device_info in api.device_list() {
-        // Check if the Vendor ID matches Elgato's.
-        if device_info.vendor_id() == ELGATO_VENDOR_ID {
-            // Get the product name, providing a default if it's not available.
-            let product_name = device_info.product_string().unwrap_or("Unknown Device");
-            println!("✅ Success! Found Stream Deck: {}", product_name);
+                // Use image-rs to load an image
+                let image = open("src/no-place-like-localhost.jpg").unwrap();
+                let alternative = image.grayscale().brighten(-50);
 
-            deck_found = true;
-            break; // Exit the loop since we found what we're looking for.
+                println!("Key count: {}", kind.key_count());
+                // Write it to the device
+                for i in 0..kind.key_count() {
+                    device.set_button_image(i, image.clone()).await.unwrap();
+                }
+
+                println!("Touch point count: {}", kind.touchpoint_count());
+                for i in 0..kind.touchpoint_count() {
+                    device.set_touchpoint_color(i, 255, 255, 255).await.unwrap();
+                }
+
+                if let Some(format) = device.kind().lcd_image_format() {
+                    let scaled_image = image.clone().resize_to_fill(
+                        format.size.0 as u32,
+                        format.size.1 as u32,
+                        image::imageops::FilterType::Nearest,
+                    );
+                    let converted_image = convert_image_with_format(format, scaled_image).unwrap();
+                    let _ = device.write_lcd_fill(&converted_image).await;
+                }
+
+                let small = match device.kind().lcd_strip_size() {
+                    Some((w, h)) => {
+                        let min = w.min(h) as u32;
+                        let scaled_image =
+                            image
+                                .clone()
+                                .resize_to_fill(min, min, image::imageops::Nearest);
+                        Some(ImageRect::from_image(scaled_image).unwrap())
+                    }
+                    None => None,
+                };
+
+                // Flush
+                device.flush().await.unwrap();
+
+                // Start new task to animate the button images
+                let device_clone = device.clone();
+                tokio::spawn(async move {
+                    let mut index = 0;
+                    let mut previous = 0;
+
+                    loop {
+                        device_clone
+                            .set_button_image(index, image.clone())
+                            .await
+                            .unwrap();
+                        device_clone
+                            .set_button_image(previous, alternative.clone())
+                            .await
+                            .unwrap();
+
+                        device_clone.flush().await.unwrap();
+
+                        sleep(Duration::from_secs_f32(0.5)).await;
+
+                        previous = index;
+
+                        index += 1;
+                        if index >= kind.key_count() {
+                            index = 0;
+                        }
+                    }
+                });
+
+                let reader = device.get_reader();
+
+                'infinite: loop {
+                    let updates = match reader.read(100.0).await {
+                        Ok(updates) => updates,
+                        Err(_) => break,
+                    };
+                    for update in updates {
+                        match update {
+                            DeviceStateUpdate::ButtonDown(key) => {
+                                println!("Button {} down", key);
+                            }
+                            DeviceStateUpdate::ButtonUp(key) => {
+                                println!("Button {} up", key);
+                                if key == device.kind().key_count() - 1 {
+                                    break 'infinite;
+                                }
+                            }
+                            DeviceStateUpdate::EncoderTwist(dial, ticks) => {
+                                println!("Dial {} twisted by {}", dial, ticks);
+                            }
+                            DeviceStateUpdate::EncoderDown(dial) => {
+                                println!("Dial {} down", dial);
+                            }
+                            DeviceStateUpdate::EncoderUp(dial) => {
+                                println!("Dial {} up", dial);
+                            }
+
+                            DeviceStateUpdate::TouchPointDown(point) => {
+                                println!("Touch point {} down", point);
+                            }
+                            DeviceStateUpdate::TouchPointUp(point) => {
+                                println!("Touch point {} up", point);
+                            }
+
+                            DeviceStateUpdate::TouchScreenPress(x, y) => {
+                                println!("Touch Screen press at {x}, {y}");
+                                if let Some(small) = &small {
+                                    device.write_lcd(x, y, small).await.unwrap();
+                                }
+                            }
+
+                            DeviceStateUpdate::TouchScreenLongPress(x, y) => {
+                                println!("Touch Screen long press at {x}, {y}")
+                            }
+
+                            DeviceStateUpdate::TouchScreenSwipe((sx, sy), (ex, ey)) => {
+                                println!("Touch Screen swipe from {sx}, {sy} to {ex}, {ey}")
+                            }
+                        }
+                    }
+                }
+
+                drop(reader);
+            }
         }
-    }
-
-    if !deck_found {
-        println!("❌ Failure: No Stream Deck device was found.");
+        Err(e) => eprintln!("Failed to create HidApi instance: {}", e),
     }
 }
+
